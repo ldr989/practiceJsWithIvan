f4cfe3b2f70dca4f4ab4c10a2fd48242caa02d3a
LDR <89.freedom@gmail.com>
2023-03-22 14:26:42 +0600

    completed the assignment for lesson 64

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const tabs = document.querySelectorAll('.tabheader__item'), 
           tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('tabheader__items');
+          tabsParent = document.querySelector('.tabheader__items');
 
     function hideTabContent () {
         tabsContent.forEach(item => {
             item.classList.add('hide');
-            item.classList.remove('show');
+            item.classList.remove('show', 'fade');
         });
 
         tabs.forEach(item => {
@@ -16,11 +16,24 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show');
+        tabsContent[i].classList.add('show', 'fade');
         tabsContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
     }
 
     hideTabContent();
     showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
 });
