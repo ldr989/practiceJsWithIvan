1b5b6910eb19cefcbc9b8046d70b259e9c5d5457
LDR <89.freedom@gmail.com>
2023-03-29 19:20:34 +0600

    added interactive in modal window

--- js/script.js
+++ js/script.js
@@ -101,4 +101,63 @@ window.addEventListener('DOMContentLoaded', ()=> {
     }
 
     setClock('.timer', deadLine);
+
+    // Modal
+
+    /* const modalBtn = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalClose = document.querySelector('.modal__close');
+
+    function showElement(element) {
+        element.style.display = 'block';
+    }
+    function hideElement(element) {
+        element.style.display = 'none';
+
+    }
+
+    modalBtn.forEach(e => {
+        e.addEventListener('click', (event) => {
+            showElement(modal);
+        });
+    });
+    modalClose.addEventListener('click', (event) => {
+        hideElement(modal);
+    }); */
+    
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+
+    function showModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+    }
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            showModal();
+        });     
+    });
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
 });
